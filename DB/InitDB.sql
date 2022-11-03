6196d14275fd1bb28013bf3bb1203054c145cfee
Yorbre25 <yraulbr25@gmail.com>
2022-11-03 08:39:20 -0600

    Creación de DB

---
+++ DB/InitDB.sql
@@ -0,0 +1,216 @@
+DROP TABLE IF EXISTS
+	Client,
+	_Order,
+	Deliveryman, 
+	Deliveryman_Phones, 
+	Product, 
+	Product_Category, 
+	Product_Photos, 
+	Order_Products, 
+	Store, 
+	Store_Phones,
+	Store_Type, 
+	Store_Products, 
+	Manager,
+	Manager_Phones
+CASCADE;
+
+
+
+create table Client
+(
+	Id int not null,
+	Name varchar(15) not null,
+	LastName1 varchar(15) not null,
+	LastName2 varchar(15),
+	Province char(15),
+	County varchar(15),
+	District varchar(15),
+    PhoneNumber varchar(10),
+    Username varchar(15) not null,
+    BirthDate date,
+	PRIMARY KEY(ID)
+);
+
+create table _Order
+(
+    Id serial,
+    Total decimal(10,2) not null,
+    ShippingAddress varchar(50) not null,
+    ClientId int not null,
+    DelManId int not null,
+    PRIMARY KEY(Id)
+);
+
+create table Deliveryman
+(
+    Id int not null,
+    Username varchar(15) not null,
+    Name varchar(15) not null,
+    LastName1 varchar(15) not null,
+    LastName2 varchar(15),
+	Email varchar(40),
+    Province char(15),
+    County varchar(15),
+    District varchar(15),
+    Password varchar(15) not null,
+    PRIMARY KEY(Id)
+);
+
+create table Deliveryman_Phones(
+    DelManId int not null,
+    PhoneNumber varchar(10) not null,
+	PRIMARY KEY (DelManId, PhoneNumber)
+);
+
+create table Product(
+    BarCode serial,
+    Price decimal(10,2) not null,
+    Name varchar(15) not null,
+    CategoryId int not null,
+    PRIMARY KEY(BarCode)
+);
+
+create table Product_Category(
+    Id serial,
+    Name varchar(15) not null,
+    PRIMARY KEY(Id)
+);
+
+create table Product_Photos(
+    ProductBarCode int not null,
+    Photo varchar(50) not null, -- AQUÍ IRÍA UNA IMAGEN
+	Primary key(ProductBarCode, Photo)
+);
+
+create table Order_Products(
+    OrderId int not null,
+    ProductBarCode int unique not null,
+    Quantity int not null,
+	Primary Key(OrderId, ProductBarCode)
+);
+
+
+create table Store(
+    Id int not null,
+    Name varchar(15) not null,
+    Email varchar(40) not null,
+    Province char(15),
+    County varchar(15),
+    District varchar(15),
+    StoreTypeId int not null,
+	ManagerId int not null, 
+    PRIMARY KEY(Id)
+);
+
+create table Store_Phones( 
+	StoreId int not null,
+	PhoneNumber varchar(40),
+	Primary Key(StoreId, PhoneNumber)
+);
+create table Store_Type(
+    Id serial,
+    Name varchar(15) not null,
+    PRIMARY KEY(Id)
+);
+
+create table Store_Products(
+    StoreId int not null,
+    ProductBarCode int not null,
+	Primary Key(StoreId, ProductBarCode)
+);
+
+create table Manager(
+    Id int not null,
+    Username varchar(15) not null,
+    Name varchar(15) not null,
+    LastName1 varchar(15) not null,
+    LastName2 varchar(15),
+    Email varchar(40) not null,
+    Province char(15),
+    County varchar(15),
+    District varchar(15),
+    Password varchar(15) not null,
+    Primary Key(Id)
+);
+
+create table Manager_Phones( 
+	ManagerId int not null,
+	PhoneNumber varchar(10) not null,
+	Primary key(ManagerId, PhoneNumber)
+);
+
+-- Foreign Keys Order
+ALTER TABLE _ORDER
+ADD CONSTRAINT ORDER_CLIENTE
+FOREIGN KEY (ClientId)
+REFERENCES Client (Id);
+
+ALTER TABLE _ORDER
+ADD CONSTRAINT ORDER_DELIVERYMAN
+FOREIGN KEY (DelManId)
+REFERENCES Deliveryman(Id);
+
+-- Foreign Keys Deliveryman_Phones
+ALTER TABLE DELIVERYMAN_PHONES
+ADD CONSTRAINT DELIVERYMAN_PHONES_DELIVERYMAN
+FOREIGN KEY (DelManId)
+REFERENCES Deliveryman (Id);
+
+--Foreign Keys Order_Products
+ALTER TABLE ORDER_PRODUCTS
+ADD CONSTRAINT ORDER_PRODUCTS_PRODUCT
+FOREIGN KEY (ProductBarCode)
+REFERENCES Product (barCode);
+
+ALTER TABLE ORDER_PRODUCTS
+ADD CONSTRAINT ORDER_PRODUCTS_ORDER
+FOREIGN KEY (OrderId)
+REFERENCES _Order (Id);
+
+--Foreign Keys Product
+ALTER TABLE PRODUCT
+ADD CONSTRAINT PRODUCT_CATEGORY
+FOREIGN KEY (CategoryId)
+REFERENCES Product_Category (Id);
+
+--Foreign Keys Product_Photos
+ALTER TABLE PRODUCT_PHOTOS
+ADD CONSTRAINT PRODUCT_PHOTOS_PRODUCT
+FOREIGN KEY (ProductBarCode)
+REFERENCES Product (BarCode);
+
+--Foreign Keys Store_Products
+ALTER TABLE STORE_PRODUCTS
+ADD CONSTRAINT STORE_PRODUCTS_PRODUCT
+FOREIGN KEY (ProductBarCode)
+REFERENCES Product (BarCode);
+
+ALTER TABLE STORE_PRODUCTS
+ADD CONSTRAINT STORE_PRODUCTS_STORE
+FOREIGN KEY (StoreId)
+REFERENCES Store (Id);
+
+--Foreign Keys Store
+ALTER TABLE STORE
+ADD CONSTRAINT STORE_STORE_TYPE
+FOREIGN KEY (StoreTypeId)
+REFERENCES Store_Type (Id);
+
+ALTER TABLE STORE
+ADD CONSTRAINT STORE_MANAGER
+FOREIGN KEY (ManagerId)
+REFERENCES Manager(Id);
+
+--Foreign Keys Store_Phones
+ALTER TABLE STORE_PHONES
+ADD CONSTRAINT STORE_STORE_PHONES
+FOREIGN KEY (StoreId)
+REFERENCES Store(Id);
+
+--Foreign Keys Manager_Phones
+ALTER TABLE MANAGER_PHONES
+ADD CONSTRAINT MANAGER_MANAGER_PHONES
+FOREIGN KEY (ManagerId)
+REFERENCES Manager(Id);
+
