2fac8e5bf9d836b369161faa959b055d8452d3c9
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2022-11-09 04:37:40 -0600

    xd

--- Backend/Data/Conexion.cs
+++ Backend/Data/Conexion.cs
@@ -5,8 +5,10 @@ namespace Backend.Data
   public class Connection
   {
     //No borren, comentenlo
-    private static string databaseURL =
-      "Server=localhost;Port=5432;Database=ubytec; User Id=postgres;Password=2019;";
+    //private static string databaseURL =
+    //  "Server=localhost;Port=5432;Database=ubytec; User Id=postgres;Password=2019;";
+
+    private static string databaseURL = "Data Source=DESKTOP-LMJ5G1R\\SQLEXPRESS;Initial Catalog=DetailTEC;Integrated Security=True";
 
     public static NpgsqlConnection Get() // Singleton
     {
---
+++ Backend/Data/DeliveryManData.cs
@@ -0,0 +1,98 @@
+﻿using Backend.Models;
+using Npgsql;
+using System.Data;
+
+namespace Backend.Data
+{
+    public class DeliveryManData
+    {
+
+        public static List<DeliveryManForGet> Listar()
+        {
+            List<DeliveryManForGet> oListaUsuario = new List<DeliveryManForGet>();
+
+            var connection = Connection.Get();
+            NpgsqlCommand cmd = new NpgsqlCommand("Get_All_DeliveryMan", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    oListaUsuario.Add(new DeliveryManForGet()
+                    {
+                        id = Convert.ToInt32(dr["Id"]),
+                        name = dr["Name"].ToString(),
+                        lastName1 = dr["LastName1"].ToString(),
+                        lastName2 = dr["LastName2"].ToString(),
+                        province = dr["Province"].ToString(),
+                        city = dr["City"].ToString(),
+                        district = dr["District"].ToString(),
+                        userName = dr["Username"].ToString(),
+                        phoneNumber = dr["PhoneNumber"].ToString(),
+
+                    });
+                }
+
+
+
+                connection.Close();
+
+                return oListaUsuario;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("Error al listar los repartidores");
+            }
+        }
+
+        public static ClienteForGet Obtener(int idusuario)
+        {
+            ClienteForGet oUsuario = new ClienteForGet();
+
+            var connection = Connection.Get();
+            NpgsqlCommand cmd = new NpgsqlCommand("Get_Client", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            // Los parámetros tienen que ir en minuscula -> Ver definición de la función
+            cmd.Parameters.AddWithValue("in_id", idusuario);
+
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    oUsuario = new ClienteForGet()
+                    {
+                        id = Convert.ToInt32(dr["Id"]),
+                        name = dr["Name"].ToString(),
+                        lastName1 = dr["LastName1"].ToString(),
+                        lastName2 = dr["LastName2"].ToString(),
+                        province = dr["Province"].ToString(),
+                        city = dr["City"].ToString(),
+                        district = dr["District"].ToString(),
+                        userName = dr["Username"].ToString(),
+                        phoneNumber = dr["PhoneNumber"].ToString(),
+                        birthdate = Convert.ToDateTime(dr["Birthdate"].ToString())
+                    };
+                }
+
+                connection.Close();
+
+                return oUsuario;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("Error al obtener el cliente");
+            }
+        }
+    }
+}
