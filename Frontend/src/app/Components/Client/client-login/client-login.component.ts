8c853f4eb04e2305d2243f62c5c3ca3a22656ea2
adri <62965634+cuadriante@users.noreply.github.com>
2022-11-12 23:25:24 -0600

    colores botones

--- Frontend/src/app/Components/Client/client-login/client-login.component.ts
+++ Frontend/src/app/Components/Client/client-login/client-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from 'src/app/Services/login-service.service';
+import { FormsService } from 'src/app/Services/forms.service';
+
 @Component({
   selector: 'app-client-login',
   templateUrl: './client-login.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    protected formsService: FormsService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit = () => {
+    this.formsService.printFormValue()
+    window.location.href = '/client'
   }
 
 }
