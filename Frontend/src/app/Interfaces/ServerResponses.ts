d312d3c464785ed4b59521b8487acab7a910d23f
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2022-11-22 20:40:01 -0600

    Servicio e interfaces para mongo

--- Frontend/src/app/Interfaces/ServerResponses.ts
+++ Frontend/src/app/Interfaces/ServerResponses.ts
@@ -3,6 +3,7 @@ import { DeliveryMan } from "./DeliveryMan"
 import { StoreType } from "./StoreType"
 import { StoreData } from "./Store"
 import { SalePerClient, SalePerStore } from "./Reports"
+import { Feedback } from "./Feedback"
 
 interface ServerResponse {
     status: 'ok' | 'error'
@@ -49,7 +50,15 @@ interface SalePerStoreResponse extends ServerResponse {
     report: SalePerStore[]
 }
 
+interface FeedbackResponse extends ServerResponse {
+    review: Feedback
+}
+
+interface FeedbacksResponse extends ServerResponse {
+    reviews: Feedback[]
+}
 export {
+
     ServerResponse,
     ManagersResponse,
     ManagerResponse,
@@ -60,5 +69,7 @@ export {
     StoresDataResponse,
     StoreDataResponse,
     SalePerClientResponse,
-    SalePerStoreResponse
+    SalePerStoreResponse,
+    FeedbackResponse,
+    FeedbacksResponse
 }
