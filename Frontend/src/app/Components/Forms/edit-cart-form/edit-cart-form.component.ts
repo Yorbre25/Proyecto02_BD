bd5f33147a6b56f9f09a131a0d751d32764e853c
adri <62965634+cuadriante@users.noreply.github.com>
2022-11-24 00:30:01 -0600

    cosa

--- Frontend/src/app/Components/Forms/edit-cart-form/edit-cart-form.component.ts
+++ Frontend/src/app/Components/Forms/edit-cart-form/edit-cart-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import Cookies from 'js-cookie';
 
+import { ProductService } from 'src/app/Services/product.service';
 @Component({
   selector: 'app-edit-cart-form',
   templateUrl: './edit-cart-form.component.html',
@@ -8,35 +9,56 @@ import Cookies from 'js-cookie';
 })
 export class EditCartFormComponent implements OnInit {
   productQuantityInCart: number[] = []
-  productNamesInCart: string[] = []
+  productIDsInCart: string[] = []
+  productNameInCart: string[] = []
+  productPriceInCart: string[] = []
 
   @Input() productsInCart?: string[]
 
   constructor(
-    
-  ) { 
+    private productService: ProductService
+  ) {
     this.productsInCart = JSON.parse(String(Cookies.get('cartProductIds'))).productIDs
 
     console.log(this.productsInCart)
 
-    for (var productID of this.productsInCart!){
-      if (this.productNamesInCart.indexOf(productID) == -1){
-        this.productNamesInCart.push(productID!)
+    for (var productID of this.productsInCart!) {
+      if (this.productIDsInCart.indexOf(productID) == -1) {
+        this.productIDsInCart.push(productID!)
         this.productQuantityInCart.push(1)
       } else {
-        let i = this.productNamesInCart.indexOf(productID)
+        let i = this.productIDsInCart.indexOf(productID)
         this.productQuantityInCart[i] = this.productQuantityInCart[i] + 1
       }
+
+
     }
 
-    console.log(this.productNamesInCart)
+    console.log(this.productIDsInCart)
     console.log(this.productQuantityInCart)
-
 
   }
 
   ngOnInit(): void {
+    for (var productID of this.productIDsInCart!) {
+      this.productService.getProduct(Number(productID))
+        .subscribe(response => {
+          if (response.status === 'error') {
+            alert(response.message)
+          }
+          else if (response.product) {
+            console.log(response.product)
+            this.productNameInCart.push(response.product.name)
+            this.productPriceInCart.push(response.product.price)
+
+          }
+          else {
+            console.log(response)
+          }
+
+        })
+    }
+
+
   }
-
-
 }
